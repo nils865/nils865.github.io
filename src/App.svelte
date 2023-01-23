c540d8be70436ffec1b52b79866b83cba5f1207b
Nils <nils.staas0806@gmail.com>
2023-01-23 19:17:51 +0100

    configured some basic stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,10 @@
-<h1>Nils Website</h1>
-<h3>Work in Progress...</h3>
+<style>
+    .main {
+        overflow: auto;
+    }
+</style>
+
+<div class="main">
+    <h1>Nils Website</h1>
+    <h3>Work in Progress...</h3>
+</div>
