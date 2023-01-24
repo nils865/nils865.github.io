7594f3dd4220e2d3c077e18fe31d95e40243596f
nils <nils.staas0806@gmail.com>
2023-01-24 17:21:56 +0100

    updated contribution graph

--- src/components/GitHub.svelte
+++ src/components/GitHub.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    let pfp: string = "img/placeholder.png";
-    let name: string = "placeholder";
-    let description: string = "placeholder";
-    let username: string = "placeholder"
+    let pfp: string = "";
+    let name: string = "";
+    let description: string = "";
+    let username: string = "";
+    let repoCount: string = "";
 
     fetch('https://api.github.com/users/nils865')
         .then((response) => response.json())
@@ -11,6 +12,7 @@
             name = data["name"];
             description = data["bio"];
             username = data["login"];
+            repoCount = data["public_repos"];
         });
 
 </script>
@@ -21,16 +23,58 @@
         <br>
         <div class="placeholder"></div>
         <a href="https://github.com/nils865" target="_blank" rel="noreferrer" class="noMP noStyle">{name}</a>
-        <p id="description" class="noMP">{description}</p>
+        <p class="description noMP">{description}</p>
     </div>
-    <img src="https://ghchart.rshah.org/843690/{username}" alt="commit-history" class="commits">
+    <div id="contributions">
+        <div class="fit-content">
+            <div id="commitbox" class="fit-content">
+                <img src="https://ghchart.rshah.org/391B74/{username}" alt="commit-history" class="commits">
+            </div>
+            <div id="repoinfo">
+                <p class="noMP">Repositiories: {repoCount}</p>
+                <div class="radio"></div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>
+    :root {
+        --pfp-size: 200px;
+        --contribution-bg: #111;
+    }
+
+    #repoinfo {
+        display: flex;
+        justify-content: center
+    }
+
+    #repoinfo > p {
+        font-size: 0.9em;
+    }
+
     .commits {
         height: 104px;
         width: 663px;
-        filter: invert(1);
+        filter: invert(0.9);
+    }
+
+    #commitbox {
+        padding: 0.5em;
+        border-style: solid;
+        border-color: var(--border);
+        border-radius: 1em;
+        background-color: var(--contribution-bg);
+    }
+
+    #contributions {
+        height: var(--pfp-size);
+        width: fit-content;
+
+        display: flex;
+        align-items: center;
+
+        margin-left: 1em;
     }
 
     #profile {
@@ -39,8 +83,8 @@
     }
 
     #pfp {
-        width: 200px;
-        height: 200px;
+        width: var(--pfp-size);
+        height: var(--pfp-size);
 
         border-radius: 35px;
     }
@@ -49,7 +93,7 @@
         font-size: 1.75rem;
     }
 
-    #description {
+    .description {
         margin-top: 0.5rem;
         font-size: 0.7rem;
         color: var(--txt2);
